27d364d5cd40f5c5f33730539e51c2f144bb381c
Supermegatiger <tigra2820@mail.ru>
2023-06-17 17:21:47 +0300

    map

--- app/page.js
+++ app/page.js
@@ -1,8 +1,12 @@
-'use client'
-
+import dynamic from 'next/dynamic'
+    // import OpenStreetMap from '../component/OpenStreetMap'
+    const Map = dynamic(() => import('../components/Map/Map'), {
+      ssr: false,
+    })
 export default function Page() {
-
   return (
-    <iframe src="https://www.openstreetmap.org/export/embed.html?bbox=38.961102962493904%2C45.02144502960909%2C38.98942708969117%2C45.03482092427761&amp;layer=mapnik"></iframe>
+    <main className='h-full'>
+      <Map />
+    </main>
   );
 }
