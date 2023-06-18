3ee080150c9ce59d502a8c676769868bc3791033
Anastasia <nyasturano@gmail.com>
2023-06-18 03:53:19 +0300

    map

--- app/page.js
+++ app/page.js
@@ -1,6 +1,6 @@
 import dynamic from 'next/dynamic'
     // import OpenStreetMap from '../component/OpenStreetMap'
-    const Map = dynamic(() => import('../Components/Map/Map'), {
+    const Map = dynamic(() => import('../Components/Map'), {
       ssr: false,
     })
 export default function Page() {
